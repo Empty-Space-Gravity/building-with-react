7ee43357f07b5c9bac9d5d1f534927ec83bc1458
___Gravity___ <111515970+Empty-Space-Gravity@users.noreply.github.com>
2025-01-26 07:12:44 +0000

    Promise in js

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("hello")
+
+new Promise = ((resolve ,reject)=>{
+    let state = 0 ;
+    if (state){
+        resolve();
+    }
+    else
+    {
+        reject
+    }
+}).then(()=>{
+    console.log("yes")
+}).catch((error)=>{
+    console.log("error");
+})
